92ab796d0d31f90f87e5e57388e2f0a5846ff305
ahmniab <124299773+ahmniab@users.noreply.github.com>
2025-07-24 01:28:49 +0300

    traslated app form

--- application-form.php
+++ application-form.php
@@ -1,14 +1,18 @@
+<?php
+require_once 'lang/init.php';
+$translations = GetTranslations(basename(__FILE__, '.php'));
+?>
 <!DOCTYPE html>
-<html dir="rtl" lang="ar">
+<html dir="<?php echo $dir; ?>" lang="<?php echo $lang; ?>">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="موقع بنك الإبداع البحرين - طلب قرض، خدمات تمويلية، دعم المشاريع الصغيرة والمتوسطة.">
+    <meta name="description" content="<?php echo $translations['meta-description']; ?>">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <title>طلب قرض - بنك الإبداع البحرين</title>
+    <title><?php echo $translations['title']; ?></title>
 
     <!-- js -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -31,8 +35,8 @@
     <section class="section" data-stellar-background-ratio="0.09" id="application">
         <div class="container">
             <div class="section-header">
-                <h2 class="section-title"> ترغب في الحصول على قرض؟ </h2>
-                <p class="section-subtitle"> ألرجاء ترك الرجاء ترك بياناتك وسيقوم أحد <br /> موضفي خدمة العملاء بالتواصل معكم في أسرع <br /> وقت ممكن لأستكمال أجرءات طلب القرض</p>
+                <h2 class="section-title"> <?php echo $translations['section-title']; ?> </h2>
+                <p class="section-subtitle"> <?php echo $translations['section-subtitle']; ?> </p>
             </div>
             <div class="row ar-dir justify-content-center">
                 <div class="col-lg-8 col-md-10">
@@ -40,15 +44,15 @@
                         <form action="php/application-process.php" id="applicationForm" method="post" class="form-vertical" data-toggle="validator">
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <label for="name_apply">الاسم الرباعي <span class="text-danger">*</span></label>
+                                    <label for="name_apply"> <?php echo $translations['name-apply']; ?> <span class="text-danger">*</span></label>
                                     <input
                                         class="form-control application-input"
-                                        data-error="الرجاء قم بإدخال الاسم بالحروف فقط بدون أرقام أو رموز"
+                                        data-error="<?php echo $translations['name-apply-error']; ?>"
                                         id="name_apply"
                                         name="name_apply"
                                         pattern="^[A-Za-z\u0600-\u06FF\s]{3,60}$"
-                                        title="الرجاء إدخال حروف فقط (عربية أو إنجليزية) بدون أرقام أو رموز، وبطول من 3 إلى 60 حرف"
-                                        placeholder="الاسم الرباعي"
+                                        title="<?php echo $translations['name-apply-title']; ?>"
+                                        placeholder="<?php echo $translations['name-apply-placeholder']; ?>"
                                         required
                                         type="text"
                                         onkeypress="return preventNumbers(event);" />
@@ -57,63 +61,74 @@
 
 
                                 <div class="form-group col-md-6">
-                                    <label for="email">البريد الإلكتروني<span class="text-danger">*</span></label>
+                                    <label for="email"> <?php echo $translations['email']; ?> <span class="text-danger">*</span></label>
                                     <input
                                         class="form-control application-input"
-                                        data-error="الرجاء ادخال البريد الالكترونى"
+                                        data-error="<?php echo $translations['email-error']; ?>"
                                         id="email"
                                         name="email"
-                                        placeholder="البريد الإلكتروني"
+                                        placeholder="<?php echo $translations['email-placeholder']; ?>"
                                         required type="email" />
                                     <div class="help-block with-errors"></div>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <label for="mobile_apply">رقم المحمول<span class="text-danger">*</span></label>
+                                    <label for="mobile_apply"> <?php echo $translations['mobile-apply']; ?> <span class="text-danger">*</span></label>
                                     <input
                                         class="form-control application-input"
-                                        data-error="الرجاء ادخال رقم المحمول"
+                                        data-error="<?php echo $translations['mobile-apply-error']; ?>"
                                         id="mobile_apply"
                                         name="mobile_apply"
-                                        placeholder="رقم المحمول"
+                                        placeholder="<?php echo $translations['mobile-apply-placeholder']; ?>"
                                         required
                                         type="text"
                                         onkeypress="return preventCharacters(event);" />
                                     <div class="help-block with-errors"></div>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label for="purpose_apply">الغاية من القرض<span class="text-danger">*</span></label>
-                                    <input class="form-control application-input" data-error="الرجاء ادخال الغاية من القرض " id="purpose_apply" name="purpose_apply" placeholder="الغاية من القرض" required type="text" />
+                                    <label for="purpose_apply"> <?php echo $translations['purpose-apply']; ?> <span class="text-danger">*</span></label>
+                                    <input class="form-control application-input" data-error="<?php echo $translations['purpose-apply-error']; ?>" id="purpose_apply" name="purpose_apply" placeholder="<?php echo $translations['purpose-apply-placeholder']; ?>" required type="text" />
                                     <div class="help-block with-errors"></div>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="type_apply">نوع القرض<span class="text-danger">*</span></label>
-                                <select class="form-control application-input" data-error="الرجاء اختيار نوع القرض" id="type_apply" name="type_apply" required>
-                                    <option value="">اختر نوع القرض المطلوب</option>
-                                    <option value="قرض الموسمي"> قرض الموسمي (100-900) دينار</option>
-                                    <option value="قرض سيدتي">قرض سيدتي (200-1000) دينار</option>
-                                    <option value="قرض الابداع">قرض الابداع (200-1000) دينار</option>
-                                    <option value="قرض النجاح">قرض النجاح (600-1400) دينار</option>
-                                    <option value="قرض الأمل">قرض الامل (1000-2400) دينار</option>
-                                    <option value="قرض التميز">قرض التميز (1500-7000) دينار</option>
-                                    <option value="قرض التفوق">قرض التفوق (3000-7000) دينار</option>
-                                    <option value="قرض التميز بلس">قرض التميز بلس (7500-10000) دينار</option>
+                                <label for="type_apply"> <?php echo $translations['type-apply']; ?> <span class="text-danger">*</span></label>
+                                <select class="form-control application-input" data-error="<?php echo $translations['type-apply-error']; ?>" id="type_apply" name="type_apply" required>
+                                    <option value=""><?php echo $translations['type-apply-placeholder']; ?></option>
+                                    <?php foreach ($translations['type-apply-options'] as $key => $value) { ?>
+                                        <option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+                                    <?php } ?>
                                 </select>
                                 <div class="help-block with-errors"></div>
                             </div>
                             <hr>
-                            <div class="text-right mb-2">
+                            <div class="mb-2">
+                                <?php if($lang == 'ar'){ ?>
                                 لمعرفة شروط تقديم الطلب يرجى
+                                <?php } else{ ?>
+                                    To view the terms and conditions, please
+                                    <?php } ?>
                                 <a href="terms.php" target="_blank" style="color:#3BA8A2; font-size:0.98rem; text-decoration:underline;">
-                                    الضغط هنا
+                                    <?php if($lang == 'ar'){ ?>
+                                        الضغط هنا
+                                    <?php } else{ ?>
+                                        Click here
+                                    <?php } ?>
                                 </a>
                             </div>
-                            <div class="text-right mb-2">
-                                للتعرف على رسوم تقديم طلبات القروض ورسوم الخدمات الاخرى التي يقدمها الإبداع الرجاء
+                            <div class="mb-2">
+                                <?php if($lang == 'ar'){ ?>
+                                    للتعرف على رسوم تقديم طلبات القروض ورسوم الخدمات الاخرى التي يقدمها الإبداع الرجاء
+                                <?php } else{ ?>
+                                    To know about loan application fees and fees for other services offered by Elbdaa, please
+                                <?php } ?>
                                 <a href="UserFiles/files/Service%20Fees_new%202020-Ar22.pdf#toolbar=0&amp;navpanes=0scrollbar=0" target="_blank" type="application/pdf" class="ml-1" style="color:#3BA8A2; text-decoration:underline;">
-                                    الضغط هنا
+                                    <?php if($lang == 'ar'){ ?>
+                                        الضغط هنا
+                                    <?php } else{ ?>
+                                        Click here
+                                    <?php } ?>
                                 </a>
                             </div>
                             <hr>
@@ -121,8 +136,19 @@
                                 <div class="arabic-checkbox">
                                     <input class="form-check-input" type="checkbox" id="arabicCheckbox2">
                                     <label class="form-check-label" for="arabicCheckbox2">
-                                     <strong>   أقر</strong>
-                                        أننى قرأت وتفهمت كافة شروط تقديم الطلب وتعتبر موافقتي عليها بمثابة إقرار نهائي غير قابل للطعن أو الرجوع فيه مستقبلا .
+                                     <strong>
+                                        <?php
+                                            if($lang == 'ar'){ ?>
+                                           أقر
+                                           <?php } else{ ?>
+                                            I acknowledge
+                                           <?php } ?>
+                                        </strong>
+                                        <?php if($lang == 'ar'){ ?>
+                                            أننى قرأت وتفهمت كافة شروط تقديم الطلب وتعتبر موافقتي عليها بمثابة إقرار نهائي غير قابل للطعن أو الرجوع فيه مستقبلا .
+                                        <?php } else{ ?>
+                                             that I have read and understood all the terms and conditions of Loan Applications and will not challenge this acknowledgement or revoke it in the future.
+                                            <?php } ?>
                                     </label>
                                 </div>
                                 <label class="form-check-label" for="arabicCheckbox2">
@@ -130,14 +156,24 @@
                             </div>
 
                             <div id="loadingMessage" style="display:none;" class="text-center text-info my-3">
-                                ⏳ جارٍ إرسال الطلب... الرجاء الانتظار.
+                                <?php if($lang == 'ar'){ ?>
+                                    ⏳ جارٍ إرسال الطلب... الرجاء الانتظار.
+                                <?php } else{ ?>
+                                    Sending the request... please wait.
+                                <?php } ?>
                             </div>
                             <div class="form-group text-center mt-3">
                                 <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response">
                             </div>
                             <div class="text-center mt-3">
                                 <div class="g-recaptcha" data-sitekey="6LetsOQZAAAAANG2gbEt7vMzNEFNHMry4QKYdVJ5"></div>
-                                <button type="submit" class="btn btn-primary btn-lg px-5" id="submitBtn">قدّم الآن</button>
+                                <button type="submit" class="btn btn-primary btn-lg px-5" id="submitBtn">
+                                    <?php if($lang == 'ar'){ ?>
+                                        قدّم الآن
+                                    <?php } else{ ?>
+                                        Submit Now
+                                    <?php } ?>
+                                </button>
                             </div>
                             <div class="h3 text-center hidden" id="msgSubmit-apply"></div>
                         </form>
