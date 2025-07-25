b5a3c8ae0f3c065d594d0fc394b724bcbbd64928
ahmniab <124299773+ahmniab@users.noreply.github.com>
2025-07-25 14:21:29 +0300

    remove unnecessary files

--- wtcp/up.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('includes/header.php');
-
-//////////////////////////////////////////////////
-
-$update = mysql_query ("UPDATE images_cat SET view_dir='1' , view_market='1' , view_req='1'   ") or die(mysql_error()); 
-
-?>
